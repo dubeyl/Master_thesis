315bdc76e741836d0ab725a9722d8c2aae63764f
dubeyl <164526068+dubeyl@users.noreply.github.com>
2025-05-20 15:27:38 +0200

    Add files via upload

---
+++ Automated_setup/Code/flymuscle-control/cpp/CMakeFiles/calibrate.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nely/flymuscle-control/cpp/src/calibrate.cpp" "CMakeFiles/calibrate.dir/calibrate.cpp.o" "gcc" "CMakeFiles/calibrate.dir/calibrate.cpp.o.d"
+  "/opt/pco/pco.cpp/pco.camera/camera.cpp" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/camera.cpp.o" "gcc" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/camera.cpp.o.d"
+  "/opt/pco/pco.cpp/pco.camera/cameraexception.cpp" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/cameraexception.cpp.o" "gcc" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/cameraexception.cpp.o.d"
+  "/opt/pco/pco.cpp/pco.camera/image.cpp" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/image.cpp.o" "gcc" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/image.cpp.o.d"
+  "/opt/pco/pco.cpp/pco.camera/stdafx.cpp" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/stdafx.cpp.o" "gcc" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/stdafx.cpp.o.d"
+  "/opt/pco/pco.cpp/pco.camera/xcite.cpp" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/xcite.cpp.o" "gcc" "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/xcite.cpp.o.d"
+  "/home/nely/flymuscle-control/cpp/src/utils/utils.cpp" "CMakeFiles/calibrate.dir/utils/utils.cpp.o" "gcc" "CMakeFiles/calibrate.dir/utils/utils.cpp.o.d"
+  "/home/nely/flymuscle-control/cpp/src/utils/utils_camera.cpp" "CMakeFiles/calibrate.dir/utils/utils_camera.cpp.o" "gcc" "CMakeFiles/calibrate.dir/utils/utils_camera.cpp.o.d"
+  "/home/nely/flymuscle-control/cpp/src/utils/utils_serial.cpp" "CMakeFiles/calibrate.dir/utils/utils_serial.cpp.o" "gcc" "CMakeFiles/calibrate.dir/utils/utils_serial.cpp.o.d"
+  "/home/nely/flymuscle-control/cpp/src/utils/utils_tracking.cpp" "CMakeFiles/calibrate.dir/utils/utils_tracking.cpp.o" "gcc" "CMakeFiles/calibrate.dir/utils/utils_tracking.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Automated_setup/Code/flymuscle-control/cpp/CMakeFiles/calibrate.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibrate.dir/calibrate.cpp.o"
+  "CMakeFiles/calibrate.dir/calibrate.cpp.o.d"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/camera.cpp.o"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/camera.cpp.o.d"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/cameraexception.cpp.o"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/cameraexception.cpp.o.d"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/image.cpp.o"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/image.cpp.o.d"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/stdafx.cpp.o"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/stdafx.cpp.o.d"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/xcite.cpp.o"
+  "CMakeFiles/calibrate.dir/opt/pco/pco.cpp/pco.camera/xcite.cpp.o.d"
+  "CMakeFiles/calibrate.dir/utils/utils.cpp.o"
+  "CMakeFiles/calibrate.dir/utils/utils.cpp.o.d"
+  "CMakeFiles/calibrate.dir/utils/utils_camera.cpp.o"
+  "CMakeFiles/calibrate.dir/utils/utils_camera.cpp.o.d"
+  "CMakeFiles/calibrate.dir/utils/utils_serial.cpp.o"
+  "CMakeFiles/calibrate.dir/utils/utils_serial.cpp.o.d"
+  "CMakeFiles/calibrate.dir/utils/utils_tracking.cpp.o"
+  "CMakeFiles/calibrate.dir/utils/utils_tracking.cpp.o.d"
+  "calibrate"
+  "calibrate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibrate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
